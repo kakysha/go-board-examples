98524494f2fb1e978fec108f090c4a33dcaa3a82
kakysha <kakysha@me.com>
2020-05-10 22:27:34 +0300

    account for ball size on walls collisions

--- modules/ball.v
+++ modules/ball.v
@@ -7,7 +7,7 @@ module ball #(parameter GAME_WIDTH = 40, parameter GAME_HEIGHT = 30) (
 	output [1:0] o_ball_direction
 );
 
-parameter BALL_SPEED = 25000000/5; // how many clock cycles for one ball move
+parameter BALL_SPEED = 25000000/40; // how many clock cycles for one ball move
 reg [24:0] r_ticks_count = 0;
 
 reg [5:0]  r_ball_pos_x = GAME_WIDTH/2;
@@ -33,13 +33,13 @@ begin
 
 				// (0,0) is top-left corner of the screen
 				
-				if (r_ball_pos_x == 0 && r_ball_direction[0:0] == 1'b0) // west
+				if (r_ball_pos_x == 1 && r_ball_direction[0:0] == 1'b0) // west
 					r_ball_direction[0:0] <= 1'b1; // change to east
-				if (r_ball_pos_x == GAME_WIDTH && r_ball_direction[0:0] == 1'b1) // east
+				if (r_ball_pos_x == GAME_WIDTH-2 && r_ball_direction[0:0] == 1'b1) // east
 					r_ball_direction[0:0] <= 1'b0; // change to west
-				if (r_ball_pos_y == 0 && r_ball_direction[1:1] == 1'b1) // north
+				if (r_ball_pos_y == 1 && r_ball_direction[1:1] == 1'b1) // north
 					r_ball_direction[1:1] <= 1'b0; // change to south
-				if (r_ball_pos_y == GAME_HEIGHT && r_ball_direction[1:1] == 1'b0) // south
+				if (r_ball_pos_y == GAME_HEIGHT-2 && r_ball_direction[1:1] == 1'b0) // south
 					r_ball_direction[1:1] <= 1'b1; // change to north
 
 				r_ball_pos_x <= r_ball_pos_x - 1 + 2 * r_ball_direction[0:0]; // -1 is west, +1 is east
